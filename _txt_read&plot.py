39e0a6a69f3c87396a6bf7dfaf29905b900be2bc
Yu-ning Chiu <n58101501@gs.ncku.edu.tw>
2022-10-02 20:10:25 +0800

    Add files via upload

--- _txt_read&plot.py
+++ _txt_read&plot.py
@@ -20,11 +20,12 @@ from IPython.display import display
 import pandas as pd
 import matplotlib.pyplot as plt
 import originpro as op
+from _atomNum_read import atomNum
 
 df_temp = pd.DataFrame()
-# Compositonal evolution ====================================
-# for i in range(1, 38):
-#     filename = './Test2/log.lammps_' + str(i)
+# # Compositonal evolution ====================================
+# for i in range(1, 37):
+#     filename = './Comp_F5V_600K_1000ps_100t/log.lammps_' + str(i)
 #     df = pd.read_csv(filename, sep='\s+', header=87, on_bad_lines='skip')
 #     df = df.drop(labels=range(2002, 2030), axis=0)
 #     df = df.astype(float, errors='raise')
@@ -35,7 +36,7 @@ df_temp = pd.DataFrame()
 
 # # Lattice Constant ====================================
 # for i in range(1001, 1002):
-#     filename = './Test2/log.lammps_' + str(i)
+#     filename = './Test_5V_600K/log.lammps_' + str(i)
 #     df = pd.read_csv(filename, sep='\s+', header=87, on_bad_lines='skip')
 #     df = df.drop(labels=range(20002, 20030), axis=0)
 #     df = df.astype(float, errors='raise')
@@ -45,18 +46,22 @@ df_temp = pd.DataFrame()
 #     df_temp = df.copy()
 
 # Structural evolution ====================================
-for i in range(1, 200):
-    filename = './Test_5V_600K/log.lammps_' + str(i)
+for i in range(1, 300):
+    # Mg_0, Al_0, Zn_0 = atomNum(1)
+    Mg, Al, Zn = atomNum(i)
+    print(i, Mg, Al, Zn)
+    filename = './X1_5V_600K/log.lammps_' + str(i)
     df = pd.read_csv(filename, sep='\s+', header=87, on_bad_lines='skip')
     df = df.drop(labels=range(2002, 2030), axis=0)
     df = df.astype(float, errors='raise')
     df['Time'] = df['Time'].map(lambda x: x + 1000.000 * (i - 1))
-    #df['TotEng'] = df['TotEng'].map(lambda x: x -(-716.377664306472) + ((-13.1645841692724) - (-12.6466222342153/9) - (-2.42554830997194/2*3)) * (i - 1))
+    df['TotEng'] = df['TotEng'].map(lambda x: x -(-716.377664306472) + ((-13.1645841692724/4*(144-Al)) + (-12.6466222342153/9*(72-Mg)) + (-2.42554830997194/2*(108-Zn))) )
+    # df['TotEng'] = df['TotEng'].map(lambda x: x - (-542.829948887566))
     df = pd.concat([df_temp, df], axis=0)
     df_temp = df.copy()
 
 df['Total Energy'] = df['TotEng'].rolling(1000, min_periods=1).mean()
-df['Moving Average'] = df['TotEng'].rolling(10000, min_periods=1).mean()
+df['Moving Average'] = df['TotEng'].rolling(10000, min_periods=0).mean()
 print(df['Moving Average'])
 ax = df.plot(x='Time', y='Total Energy', figsize=(15,5), color='g', alpha=0.5)
 df.plot(x='Time', y='Moving Average', figsize=(15,5), color='k', linewidth=2.5, ax=ax)
@@ -68,7 +73,7 @@ df.plot(x='Time', y='Moving Average', figsize=(15,5), color='k', linewidth=2.5, 
 # plt.xlim(0, 100000)
 # plt.yticks([-700, -710, -720, -730], fontname = 'Helvetica')
 plt.xlabel('Simulation time (fs)', fontsize=18, fontname = 'Helvetica')
-plt.ylabel('Total energy (eV/lattice)', fontsize=18, fontname = 'Helvetica')
+plt.ylabel('Reaction energy (eV/lattice)', fontsize=18, fontname = 'Helvetica')
 plt.show()
 
 # op.set_show()
